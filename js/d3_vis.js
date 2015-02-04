b066dba9a58e571e167067af79adb467101a9396
shenzy0613 <szy91@gatech.edu>
2015-02-04 13:31:15 -0500

    Add day before and later

--- js/d3_vis.js
+++ js/d3_vis.js
@@ -1,12 +1,9 @@
 var margin = {top: 15, right: 10, bottom: 10, left: 0},
     width = $("#figure1").width() - margin.left - margin.right,
-    height = 150 - margin.top - margin.bottom;
+    height = 260 - margin.top - margin.bottom;
 
-drawNewVis("total_data.csv","raw_data.csv");
-
-function drawNewVis(file1,file2){
-	d3.csv(file1, function(data) {
-	  var datatotal = data.map(function(d) { return [+d["Lowest Price"], d["From"], d["To"], d["Earliest Departure"], d["Latest Arrival"] ]; });
+	d3.csv("total_data.csv", function(data) {
+	  var datatotal = data.map(function(d) { return [+d["Lowest Price"], d["From"], d["To"], d["Earliest Departure"], d["Latest Arrival"], d["Date"] ]; });
 
 	  var x = d3.time.scale()
 	    .domain([new Date(datatotal[0][3]), new Date(datatotal[0][4])])
@@ -20,12 +17,12 @@ function drawNewVis(file1,file2){
 
 	  var tip = d3.tip()
 	    .attr('class', 'd3-tip')
-	    .offset([120, 0])
+	    .offset([145, 0])
 	    .html(function(d) {
-	      var departHours = (new Date(d[4])).getHours();
-	      var departMinutes = (new Date(d[4])).getMinutes();
-	      var arriveHours = (new Date(d[5])).getHours();
-	      var arriveMinutes = (new Date(d[5])).getMinutes();
+	      var departHours = (new Date(d[5])).getHours();
+	      var departMinutes = (new Date(d[5])).getMinutes();
+	      var arriveHours = (new Date(d[6])).getHours();
+	      var arriveMinutes = (new Date(d[6])).getMinutes();
 
 	      if(departMinutes<10)
 	        var departMinutesString = '0' + departMinutes;
@@ -40,10 +37,16 @@ function drawNewVis(file1,file2){
 	      return "Flight Number:&nbsp; <span style='color:yellow;float:right;'>" +  d[0] + "</span><br>"
 	              +"Depart time: <span style='color:yellow;float:right;'>" + departHours +":"+departMinutesString + "</span><br>"
 	              +"Arrive time: <span style='color:yellow;float:right;'>" + arriveHours +":"+arriveMinutesString + "</span><br>"
-	              +"Aircraft type: <span style='color:yellow;float:right;'>" + d[6] + "</span><br>";
+	              +"Aircraft type: <span style='color:yellow;float:right;'>" + d[7] + "</span><br>"
+	              +"Price: <span style='color:yellow;float:right;'>$" + d[2] + "</span><br>";
 	  })
 
-	  var space=[new Date("2000-01-01T00:00-0400"),new Date("2000-01-01T00:00-0400"),new Date("2000-01-01T00:00-0400"),new Date("2000-01-01T00:00-0400"),new Date("2000-01-01T00:00-0400"),new Date("2000-01-01T00:00-0400")];
+	  var space=[new Date("2000-01-01T00:00-0400"),new Date("2000-01-01T00:00-0400"),
+	  			 new Date("2000-01-01T00:00-0400"),new Date("2000-01-01T00:00-0400"),
+	  			 new Date("2000-01-01T00:00-0400"),new Date("2000-01-01T00:00-0400"),
+	  			 new Date("2000-01-01T00:00-0400"),new Date("2000-01-01T00:00-0400"),
+	  			 new Date("2000-01-01T00:00-0400"),new Date("2000-01-01T00:00-0400"),
+	  			 new Date("2000-01-01T00:00-0400"),new Date("2000-01-01T00:00-0400")];
 
 	  var svg = d3.select("#figure1").append("svg")
 	      .attr("width", width + margin.left + margin.right)
@@ -54,38 +57,99 @@ function drawNewVis(file1,file2){
 
 	  svg.call(tip);
 
-	  d3.csv(file2, function(data) {
-	    var dataset = data.map(function(d) { return [ d["Flight Number"], +d["Price"], d["From"], d["To"], d["Departure"], d["Arrival"], d["Aircraft Type"] ]; });
+	  
+
+	  d3.csv("raw_data.csv", function(data) {
+	    var dataset = data.map(function(d) { return [ d["Flight Number"], d["Date"], +d["Price"], d["From"], d["To"], d["Departure"], d["Arrival"], d["Aircraft Type"] ]; });
 
 	    for(i=0;i<dataset.length;i++) {
-	      if ( (new Date(dataset[i][4]).getTime() - space[0].getTime()) > 0 ) {
-	        dataset[i][7] = 0;
-	        space[0] = new Date((new Date(dataset[i][5])).getTime()+600000);
-	      }
-	      else if( (new Date(dataset[i][4]).getTime() - space[1].getTime()) > 0 ) {
-	        dataset[i][7] = 1;
-	        space[1] = new Date((new Date(dataset[i][5])).getTime()+600000);
-	      }
-	      else if( (new Date(dataset[i][4]).getTime() - space[2].getTime()) > 0 ) {
-	        dataset[i][7] = 2;
-	        space[2] = new Date((new Date(dataset[i][5])).getTime()+600000);
-	      }
-	      else if( (new Date(dataset[i][4]).getTime() - space[3].getTime()) > 0 ) {
-	        dataset[i][7] = 3;
-	        space[3] = new Date((new Date(dataset[i][5])).getTime()+600000);
-	      }
+	    	if(dataset[i][1] == datatotal[0][5]){
+	    		if ( (new Date(dataset[i][5]).getTime() - space[0].getTime()) > 0 ) {
+			        dataset[i][8] = 0;
+			        space[0] = new Date((new Date(dataset[i][6])).getTime()+600000);
+			      }
+			      else if( (new Date(dataset[i][5]).getTime() - space[1].getTime()) > 0 ) {
+			        dataset[i][8] = 1;
+			        space[1] = new Date((new Date(dataset[i][6])).getTime()+600000);
+			      }
+			      else if( (new Date(dataset[i][5]).getTime() - space[2].getTime()) > 0 ) {
+			        dataset[i][8] = 2;
+			        space[2] = new Date((new Date(dataset[i][6])).getTime()+600000);
+			      }
+			      else if( (new Date(dataset[i][5]).getTime() - space[3].getTime()) > 0 ) {
+			        dataset[i][8] = 3;
+			        space[3] = new Date((new Date(dataset[i][6])).getTime()+600000);
+			      }
+	    	}
+	    	else if(new Date(dataset[i][5]).getTime() < new Date(datatotal[0][5]).getTime()) {
+	    		if ( (new Date(dataset[i][5]).getTime() - space[4].getTime()) > 0 ) {
+			        dataset[i][8] = 0;
+			        space[4] = new Date((new Date(dataset[i][6])).getTime()+600000);
+			      }
+			      else if( (new Date(dataset[i][5]).getTime() - space[5].getTime()) > 0 ) {
+			        dataset[i][8] = 1;
+			        space[5] = new Date((new Date(dataset[i][6])).getTime()+600000);
+			      }
+			      else if( (new Date(dataset[i][5]).getTime() - space[6].getTime()) > 0 ) {
+			        dataset[i][8] = 2;
+			        space[6] = new Date((new Date(dataset[i][6])).getTime()+600000);
+			      }
+			      else if( (new Date(dataset[i][5]).getTime() - space[7].getTime()) > 0 ) {
+			        dataset[i][8] = 3;
+			        space[7] = new Date((new Date(dataset[i][6])).getTime()+600000);
+			      }
+	    	}
+	    	else if(new Date(dataset[i][5]).getTime() > new Date(datatotal[0][5]).getTime()) {
+	    		if ( (new Date(dataset[i][5]).getTime() - space[8].getTime()) > 0 ) {
+			        dataset[i][8] = 0;
+			        space[8] = new Date((new Date(dataset[i][6])).getTime()+600000);
+			      }
+			      else if( (new Date(dataset[i][5]).getTime() - space[9].getTime()) > 0 ) {
+			        dataset[i][8] = 1;
+			        space[9] = new Date((new Date(dataset[i][6])).getTime()+600000);
+			      }
+			      else if( (new Date(dataset[i][5]).getTime() - space[10].getTime()) > 0 ) {
+			        dataset[i][8] = 2;
+			        space[10] = new Date((new Date(dataset[i][6])).getTime()+600000);
+			      }
+			      else if( (new Date(dataset[i][5]).getTime() - space[11].getTime()) > 0 ) {
+			        dataset[i][8] = 3;
+			        space[11] = new Date((new Date(dataset[i][6])).getTime()+600000);
+			      }
+	    	}
 	    }
+
+	    console.log(dataset);
 
 	    svg.selectAll('.chart')
 	      .data(dataset)
 	      .enter().append('rect')
 	      .attr('class', 'bar')
-	      .attr('x', function(d) { return x(new Date(d[4])) })
-	      .attr('y', function(d) { return margin.top+10+d[7]*30})
-	      .attr('height', 25)
-	      .attr('width', function(d) {return (x(new Date(d[5]))-x(new Date(d[4])))})
+	      .attr('x', function(d) { 
+			if(d[1] == datatotal[0][5]) 
+	      		return x(new Date(d[5])) 
+	      	else if (new Date(d[1]).getTime() < new Date(datatotal[0][5]).getTime())
+	      		return x(new Date(new Date(d[5]).getTime()+86400000))
+	      	else
+	      		return x(new Date(new Date(d[5]).getTime()-86400000)) 
+	      })	
+	      .attr('y', function(d) { 
+	      	if(d[1] == datatotal[0][5]) 
+	      		return margin.top+10+d[8]*30
+	      	else if (new Date(d[1]).getTime() < new Date(datatotal[0][5]).getTime())
+	      		return margin.top+120+d[8]*15
+	      	else
+	      		return margin.top+180+d[8]*15
+	      })
+	      .attr('height', function(d) { 
+			if(d[1] == datatotal[0][5]) 
+	      		return 25
+	      	else 
+	      		return 10
+	      })	
+	      .attr('width', function(d) {return (x(new Date(d[6]))-x(new Date(d[5])))})
 	      .style("fill", function(d) {
-	        if(d[1]==datatotal[0][0]){
+	        if(d[2]==datatotal[0][0]){
 	          return "#5EB72A";
 	        }
 	        else 
@@ -93,32 +157,60 @@ function drawNewVis(file1,file2){
 	      })
 	      .on('mouseover', tip.show)
 	      .on('mouseout', tip.hide)
+
 	    svg.selectAll('.chart')
 	      .data(dataset)
 	      .enter().append('text')
 	      .attr({
-	          'x': function(d) { return x(new Date(d[4]))+3},
-	          'y': function(d) { return margin.top+27+d[7]*30},
+	          'x': function(d) { return x(new Date(d[5]))+3},
+	          'y': function(d) { return margin.top+27+d[8]*30},
 	          'class': 'legend-label'
 	      })
-	      .text(function(d) {return "$"+d[1]});
+	      .text(function(d) {return "$"+d[2]});
 	    
 	    svg.append('g')
 	      .attr('class', 'x axis')
 	      .attr('transform', 'translate(0, ' +  margin.top + ')')
 	      .call(xAxis);
+
+	    svg.append("line")
+          .attr("x1", 0)
+          .attr("y1", 120)
+          .attr("x2", width)
+          .attr("y2", 120)
+          .style("fill", "none")
+	      .style("stroke", "#000")
+	      .style("shape-rendering", "crispEdges")
+
+	    svg.append("text")
+	      .attr("x", 0)
+          .attr("y", 135)
+          .attr({"font-size":9})
+	      .text("Depart 1 day earlier")
+
+	    svg.append("line")
+          .attr("x1", 0)
+          .attr("y1", 180)
+          .attr("x2", width)
+          .attr("y2", 180)
+          .style("fill", "none")
+	      .style("stroke", "#000")
+	      .style("shape-rendering", "crispEdges")
+
+	    svg.append("text")
+	      .attr("x", 0)
+          .attr("y", 195)
+          .attr({"font-size":9})
+	      .text("Depart 1 day later")
 	  });
 	});
-}
 
-
-
-  d3.selectAll(".axis path")
+	d3.selectAll(".axis path")
       .style("fill", "none")
       .style("stroke", "#000")
       .style("shape-rendering", "crispEdges")
 
-  d3.selectAll(".axis line")
+	d3.selectAll(".axis line")
       .style("fill", "none")
       .style("stroke", "#000")
       .style("shape-rendering", "crispEdges")
